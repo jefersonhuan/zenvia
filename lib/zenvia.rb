cb7298d65a874961cd8a394d1a63616ed9b0fc62
Jeferson Huan <jeferson.hsa@gmail.com>
2015-12-28 20:23:27 -0200

    Initial commit

---
+++ lib/zenvia.rb
@@ -0,0 +1,43 @@
+require "zenvia/version"
+require 'httparty'
+require 'base64'
+require 'json'
+
+class Zenvia
+  def initialize(user, code)
+    return puts 'User and code must no be nil' if user.nil? or code.nil?
+    @auth = Base64.encode64("#{user}:#{code}").strip
+  end
+
+  attr_writer :auth
+
+  def send_message(from = nil, to, message)
+    return puts 'The receiver must not be nil' if to.nil?
+    to = to.to_s unless to.class.eql? String
+    return puts 'The to-number must have just numbers!' unless /\d*/.match(to)
+    response = send_sms(from, to, message)
+    print 'JSON response: ' + response.body
+  end
+
+  private
+  def send_sms(from = nil, to, message)
+    to.insert(0, '55') unless /^55/.match(to)
+    endpoint = 'https://api-rest.zenvia360.com.br/services/send-sms'
+    HTTParty.post(endpoint,
+                  body: {
+                      sendSmsRequest: {
+                          from: from,
+                          to: to,
+                          schedule: Time.now.strftime("%Y-%m-%dT%H:%M:%S"),
+                          msg: message,
+                          callbackOption: 'NONE'
+                      }
+                  }.to_json,
+                  headers: {
+                      'Content-Type' => 'application/json',
+                      'Authorization' => "Basic #{@auth}",
+                      'Accept' => 'application/json'
+                  }
+    )
+  end
+end
---
+++ lib/zenvia/version.rb
@@ -0,0 +1,3 @@
+class Zenvia
+  VERSION = "0.0.0"
+end
